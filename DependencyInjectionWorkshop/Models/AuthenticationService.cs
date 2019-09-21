9dd4e71b77eb57722f83becc29f0577ed56388da
Joey Chen <jay.hatelove@gmail.com>
2019-09-21 10:27:00 +0800

    add Notify() feature

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using Dapper;
+using SlackAPI;
 
 namespace DependencyInjectionWorkshop.Models
 {
@@ -44,6 +45,10 @@ namespace DependencyInjectionWorkshop.Models
             }
             else
             {
+                string message = $"{account} try to login failed";
+                var slackClient = new SlackClient("my api token");
+                slackClient.PostMessage(response1 => { }, "my channel", message, "my bot name");
+
                 return false;
             }
         }
