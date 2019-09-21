b93369634d3e4de960bcf639745824afc6241980
Joey Chen <jay.hatelove@gmail.com>
2019-09-21 14:25:53 +0800

    inline httpclient parameter and extract class NLogAdapter

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -74,23 +74,6 @@ namespace DependencyInjectionWorkshop.Models
             addFailedCountResponse.EnsureSuccessStatusCode();
         }
 
-        public void ResetFailedCount(string accountId)
-        {
-            var resetResponse = new HttpClient() {BaseAddress = new Uri("http://joey.com/")}
-                                .PostAsJsonAsync("api/failedCounter/Reset", accountId).Result;
-            resetResponse.EnsureSuccessStatusCode();
-        }
-
-        public int GetFailedCount(string accountId, HttpClient httpClient)
-        {
-            var failedCountResponse =
-                httpClient.PostAsJsonAsync("api/failedCounter/GetFailedCount", accountId).Result;
-            failedCountResponse.EnsureSuccessStatusCode();
-
-            var failedCount = failedCountResponse.Content.ReadAsAsync<int>().Result;
-            return failedCount;
-        }
-
         public bool GetAccountIsLocked(string accountId)
         {
             var isLockedResponse = new HttpClient() {BaseAddress = new Uri("http://joey.com/")}
@@ -100,15 +83,47 @@ namespace DependencyInjectionWorkshop.Models
             var isLocked = isLockedResponse.Content.ReadAsAsync<bool>().Result;
             return isLocked;
         }
+
+        public int GetFailedCount(string accountId)
+        {
+            var failedCountResponse =
+                new HttpClient() {BaseAddress = new Uri("http://joey.com/")}
+                    .PostAsJsonAsync("api/failedCounter/GetFailedCount", accountId).Result;
+            failedCountResponse.EnsureSuccessStatusCode();
+
+            var failedCount = failedCountResponse.Content.ReadAsAsync<int>().Result;
+            return failedCount;
+        }
+
+        public void ResetFailedCount(string accountId)
+        {
+            var resetResponse = new HttpClient() {BaseAddress = new Uri("http://joey.com/")}
+                                .PostAsJsonAsync("api/failedCounter/Reset", accountId).Result;
+            resetResponse.EnsureSuccessStatusCode();
+        }
+    }
+
+    public class NLogAdapter
+    {
+        public NLogAdapter()
+        {
+        }
+
+        public void LogMessage(string message)
+        {
+            var logger = NLog.LogManager.GetCurrentClassLogger();
+            logger.Info(message);
+        }
     }
 
     public class AuthenticationService
     {
+        private readonly FailedCounter _failedCounter;
+        private readonly NLogAdapter _nLogAdapter;
         private readonly OtpService _otpService;
         private readonly ProfileDao _profileDao;
         private readonly Sha256Adapter _sha256Adapter;
         private readonly SlackAdapter _slackAdapter;
-        private readonly FailedCounter _failedCounter;
 
         public AuthenticationService()
         {
@@ -117,6 +132,7 @@ namespace DependencyInjectionWorkshop.Models
             _otpService = new OtpService();
             _slackAdapter = new SlackAdapter();
             _failedCounter = new FailedCounter();
+            _nLogAdapter = new NLogAdapter();
         }
 
         public bool Verify(string accountId, string password, string otp)
@@ -143,25 +159,13 @@ namespace DependencyInjectionWorkshop.Models
             {
                 _failedCounter.AddFailedCount(accountId);
 
-                LogFailedCount(accountId);
+                var failedCount = _failedCounter.GetFailedCount(accountId); 
+                _nLogAdapter.LogMessage($"accountId:{accountId} failed times:{failedCount}");
 
                 _slackAdapter.Notify(accountId);
 
                 return false;
             }
-        }
-
-        private void LogFailedCount(string accountId)
-        {
-            var failedCount = _failedCounter.GetFailedCount(accountId, new HttpClient() {BaseAddress = new Uri("http://joey.com/")});
-
-            LogMessage($"accountId:{accountId} failed times:{failedCount}");
-        }
-
-        private static void LogMessage(string message)
-        {
-            var logger = NLog.LogManager.GetCurrentClassLogger();
-            logger.Info(message);
         }
     }
 
