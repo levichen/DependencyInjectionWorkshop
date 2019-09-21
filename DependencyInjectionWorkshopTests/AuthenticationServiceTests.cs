cdcefec71bb21e840098f3eb6544ed7291c0b37d
levichen <levichen@nine-yi.com>
2019-09-21 16:59:01 +0800

    Add Test Case: IsValid

--- DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
+++ DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
@@ -1,4 +1,6 @@
 ﻿using DependencyInjectionWorkshop.Models;
+using DependencyInjectionWorkshop.Repos;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace DependencyInjectionWorkshopTests
@@ -6,10 +8,68 @@ namespace DependencyInjectionWorkshopTests
     [TestFixture]
     public class AuthenticationServiceTests
     {
+        
+        private const string DefaultAccountId = "joey";
+        private const string DefaultHashedPassword = "my hashed password";
+        private const string DefaultInputPassword = "abc";
+        private const string DefaultOtp = "123456";
+        
+        private IProfile _profile;
+        private IHash _hash;
+        private IOtpService _otpService;
+        private IFailedCounter _failedCounter;
+        private INotification _slackAdapter;
+        private AuthenticationService _authenticationService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _profile = Substitute.For<IProfile>();
+            _hash = Substitute.For<IHash>();
+            _otpService = Substitute.For<IOtpService>();
+
+            _failedCounter = Substitute.For<IFailedCounter>();
+            _slackAdapter = Substitute.For<INotification>();
+
+            _authenticationService = new AuthenticationService(_profile, _hash, _otpService, _slackAdapter, _failedCounter, Substitute.For<ILogger>());
+        }
+        
         [Test]
         public void is_valid()
         {
-            var authenticationService = new AuthenticationService();
+            GivenPassword(DefaultAccountId, DefaultHashedPassword);
+            GivenHash(DefaultInputPassword, DefaultHashedPassword); 
+            GivenOtp(DefaultAccountId, DefaultOtp);
+
+            var isValid = WhenVerify(DefaultAccountId, DefaultInputPassword, DefaultOtp);
+
+            ShouldBeValid(isValid);
+        }
+        
+        
+        private static void ShouldBeValid(bool isValid)
+        {
+            Assert.IsTrue(isValid);
+        }
+
+        private bool WhenVerify(string accountId, string password, string otp)
+        {
+            return _authenticationService.Verify(accountId, password, otp);
+        }
+
+        private void GivenOtp(string accountId, string otp)
+        {
+            _otpService.GetCurrentOtp(accountId).Returns(otp);
+        }
+
+        private void GivenHash(string inputPassword, string hashedPassword)
+        {
+            _hash.Compute(inputPassword).Returns(hashedPassword);
+        }
+
+        private void GivenPassword(string accountId, string password)
+        {
+            _profile.GetPassword(accountId).Returns(password);
         }
     }
 }
