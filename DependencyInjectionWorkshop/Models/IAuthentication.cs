16fb75defd21f1199196401550c774b31201eef5
Joey Chen <jay.hatelove@gmail.com>
2019-09-22 17:00:54 +0800

    intercepted by AuditLogAttribute

--- DependencyInjectionWorkshop/Models/IAuthentication.cs
+++ DependencyInjectionWorkshop/Models/IAuthentication.cs
@@ -1,7 +1,14 @@
-﻿namespace DependencyInjectionWorkshop.Models
+﻿using System;
+
+namespace DependencyInjectionWorkshop.Models
 {
     public interface IAuthentication
     {
+        [AuditLog]
         bool Verify(string accountId, string password, string otp);
+    }
+
+    public class AuditLogAttribute : Attribute
+    {
     }
 }
