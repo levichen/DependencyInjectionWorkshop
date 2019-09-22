9b6942a78d77c1d787a80076b857ff72c9e8b6cd
Joey Chen <jay.hatelove@gmail.com>
2019-09-22 15:44:52 +0800

    refactor

--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -49,24 +49,6 @@ namespace MyConsole
         }
     }
 
-    internal class LogMethodInfoDecorator : AuthenticationBaseDecorator
-    {
-        private readonly ILogger _logger;
-
-        public LogMethodInfoDecorator(IAuthentication authentication, ILogger logger) : base(authentication)
-        {
-            _logger = logger;
-        }
-
-        public override bool Verify(string accountId, string password, string otp)
-        {
-            _logger.Info($"{accountId} | {password} | {otp}");
-            var isValid = base.Verify(accountId, password, otp);
-            _logger.Info($"isValid:{isValid}");
-            return isValid;
-        }
-    }
-
     internal class FakeLogger : ILogger
     {
         public void Info(string message)
