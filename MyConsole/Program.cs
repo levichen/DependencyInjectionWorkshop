f968aa5ae0e62143791954c464af78ced42159f8
Joey Chen <jay.hatelove@gmail.com>
2019-09-22 14:44:47 +0800

    use autofac for register and resolve with decorator

--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autofac;
 using DependencyInjectionWorkshop.Models;
 using DependencyInjectionWorkshop.Repositories;
 
@@ -10,31 +11,41 @@ namespace MyConsole
 {
     class Program
     {
-        private static INotification _notification;
-        private static ILogger _logger;
-        private static IOtpService _otpService;
-        private static IFailedCounter _failedCounter;
-        private static IHash _hash;
-        private static IProfile _profile;
         private static IAuthentication _authentication;
+        private static IContainer _container;
 
         static void Main(string[] args)
         {
-            _notification = new FakeSlack();
-            _logger = new FakeLogger();
-            _failedCounter = new FakeFailedCounter();
-            _otpService = new FakeOtp();
-            _hash = new FakeHash();
-            _profile = new FakeProfile();
-            _authentication =
-                new AuthenticationService(_otpService, _profile, _hash);
+            RegisterContainer();
 
-            _authentication = new NotificationDecorator(_authentication, _notification);
-            _authentication = new FailedCounterDecorator(_authentication, _failedCounter);
-            _authentication = new LogFailedCountDecorator(_authentication, _logger, _failedCounter);
+            _authentication = _container.Resolve<IAuthentication>();
+
 
             var isValid = _authentication.Verify("joey", "abc", "wrong otp");
             Console.WriteLine($"result:{isValid}");
+        }
+
+        private static void RegisterContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<FakeProfile>().As<IProfile>();
+            builder.RegisterType<FakeHash>().As<IHash>();
+            builder.RegisterType<FakeOtp>().As<IOtpService>();
+            builder.RegisterType<NLogAdapter>().As<ILogger>();
+            builder.RegisterType<FakeSlack>().As<INotification>();
+            builder.RegisterType<FakeFailedCounter>().As<IFailedCounter>();
+            builder.RegisterType<AuthenticationService>().As<IAuthentication>();
+
+            builder.RegisterDecorator<NotificationDecorator, IAuthentication>();
+            builder.RegisterDecorator<FailedCounterDecorator, IAuthentication>();
+            builder.RegisterDecorator<LogFailedCountDecorator, IAuthentication>();
+
+            //_authentication = new NotificationDecorator(_authentication, _notification);
+            //_authentication = new FailedCounterDecorator(_authentication, _failedCounter);
+            //_authentication = new LogFailedCountDecorator(_authentication, _logger, _failedCounter);
+            var container = builder.Build();
+            _container = container;
         }
     }
 
